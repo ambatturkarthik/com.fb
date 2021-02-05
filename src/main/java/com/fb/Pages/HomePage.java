ebcbb1a9315b49e614c32af80f6e4eb5d598314a
ambatturkarthik <78400582+ambatturkarthik@users.noreply.github.com>
2021-02-05 10:50:44 +0530

    Update HomePage.java

--- src/main/java/com/fb/Pages/HomePage.java
+++ src/main/java/com/fb/Pages/HomePage.java
@@ -3,5 +3,10 @@ package com.fb.Pages;
 import com.fb.Name.Testbase;
 
 public class HomePage extends Testbase{
+  
+  public static void main(String[] args)
+  {
+    System.Out.Println("This is Homepage");
+  }
 
 }
